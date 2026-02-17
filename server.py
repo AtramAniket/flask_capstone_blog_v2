c728ec344154a47072c39f08385473215bb7d3f4
AtramAniket <aniket_atram@live.in>
2026-02-17 15:54:02 +0530

    fixed the css styling not being applied due to relative path issue

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def contact_me():
 @app.route("/post/<int:post_id>")
 def get_post(post_id):
     post = blog.get_post_by_id(post_id)
-    return render_template("post.html", blog_post = post)
+    return render_template("post.html", blog_post = post[0])
 
 if __name__ == "__main__":
     app.run(debug = True)
